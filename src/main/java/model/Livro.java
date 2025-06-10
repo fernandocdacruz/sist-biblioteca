41c6da0634f10763072fee261690a758a54313f6
Fernando Campos da Cruz <fernandocdacruz@gmail.com>
2025-06-10 11:15:05 -0300

    POJO livro criado

--- src/main/java/model/Livro.java
+++ src/main/java/model/Livro.java
@@ -1,5 +1,58 @@
 package model;
 
 public class Livro {
+	
+	private String titulo;
+	private String autor;
+	private String ano_de_publicacao;
+	private String isbn;
+	
+	public Livro() {
+	}
 
+	public Livro(String titulo, String autor, String ano_de_publicacao, String isbn) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.ano_de_publicacao = ano_de_publicacao;
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getAno_de_publicacao() {
+		return ano_de_publicacao;
+	}
+
+	public void setAno_de_publicacao(String ano_de_publicacao) {
+		this.ano_de_publicacao = ano_de_publicacao;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [titulo=" + titulo + ", autor=" + autor + ", ano_de_publicacao=" + ano_de_publicacao + ", isbn="
+				+ isbn + "]";
+	}
+	
 }
